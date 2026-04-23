3923adab4bd8d4f47acce5cc95a22cb5ba847082
namith-1 <nrnr1nrnr11@gmail.com>
2026-04-24 04:00:56 +0530

    fix(OAuth): google auth error fixed

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ const Student = require('../models/Student');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL: `${process.env.BACKEND_URL || "http://localhost:5000"}/api/auth/google/callback`,
     passReqToCallback: true
   },
   async (req, accessToken, refreshToken, profile, done) => {
